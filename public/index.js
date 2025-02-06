87d7323aabebb2171de320c4f08b9666a7c1c6d9
Nanashi <a@techz.jp>
2025-02-06 23:50:13 +0900

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8" />
+  <title>Discord OAuth Extended</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <div class="container">
+    <h1>ログインページ</h1>
+    <p>Discord OAuth + IP / 国 情報を取得します。</p>
+    <button id="loginBtn">Discordでログイン</button>
+  </div>
+
+  <script src="main.js"></script>
+</body>
+</html>
